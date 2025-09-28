871040f8a7f54b77f12efbda13f2088696b830fc
KThompson2002 <kylezthompson@gmail.com>
2025-09-27 21:16:28 -0500

    Fixed Turtle movement with P controller and straight line movement. Allowed turtle to move to different waypoints.

--- ws/src/homework1/turtle_control/turtle_control/waypoint.py
+++ ws/src/homework1/turtle_control/turtle_control/waypoint.py
@@ -7,7 +7,7 @@ from turtle_interfaces.srv import Waypoint
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from turtlesim_msgs.srv import SetPen, TeleportAbsolute
 from turtlesim_msgs.msg import Pose
-from geometry_msgs.msg import Twist, Vector3
+from geometry_msgs.msg import Twist, Vector3, Point
 
 
 class State(Enum):
@@ -15,6 +15,7 @@ class State(Enum):
         Determines what the main timer function should be doing on each iteration
     """
     MOVING = auto(),
+    LAST = auto()
     STOPPED = auto(),
 
 class WaypointNode(Node):
@@ -22,54 +23,86 @@ class WaypointNode(Node):
         super().__init__("waypoint")
         self.freq = 90 # hz
         self._velocity = 2.0
+        self._vel_ang = 4.0
         self.timer = self.create_timer(1/self.freq, self.timer_callback)
         self.my_callback_group = MutuallyExclusiveCallbackGroup()
-        #Create services
+        
+        # Create services
         self.toggle = self.create_service(Empty, "toggle",  self.toggle_callback)
         self.load = self.create_service(Waypoint, "load", self.load_callback)
         # Establish initial state
         self.state = State.STOPPED
         self.curr_waypoint = None
+        self.curr_waypoint_idx = 0
+        self.waypoints = None
 
-        #Creating clients
+        #
+        self.declare_parameter("tolerance", 1.0)
+        # Create Parameter
+        self.tolerance = self.get_parameter("tolerance").get_parameter_value().double_value
+
+        # Creating clients
         self.reset = self.create_client(Empty, "/reset", callback_group=self.my_callback_group)
         self.set_pen = self.create_client(SetPen, "turtle1/set_pen", callback_group=self.my_callback_group)
         self.teleport = self.create_client(TeleportAbsolute, "turtle1/teleport_absolute", callback_group=self.my_callback_group)
 
-        #Establish future client storage
+        # Establish future client storage
         self.set_pen_msg = SetPen.Request()
         self.teleport_msg = TeleportAbsolute.Request()
 
-        #Create Subscribers
+        # Create Subscribers
         self.pose = Pose()
         self._feedback = self.create_subscription(Pose, "turtle1/pose", self.feedback_callback, 10)
 
-        #Create Publishers
+        # Create Publishers
         self._pub = self.create_publisher(Twist, "cmd_vel", 10)
 
         if not self.reset.wait_for_service(timeout_sec=1.0):
-            raise RuntimeError('Timeout waiting for "kill" service to become available')
+            raise RuntimeError('Timeout waiting for "reset" service to become available')
 
         if not self.set_pen.wait_for_service(timeout_sec=1.0):
-            raise RuntimeError('Timeout waiting for "spawn" service to become available')
+            raise RuntimeError('Timeout waiting for "SetPen" service to become available')
 
         if not self.teleport.wait_for_service(timeout_sec=1.0):
-            raise RuntimeError('Timeout waiting for "spawn" service to become available')
+            raise RuntimeError('Timeout waiting for "Teleport Absolute" service to become available')
 
 
     def timer_callback(self):
-        if self.state == State.MOVING:
+        if self.state == State.MOVING or self.state == State.LAST:
             msg = f"moving"
             self.get_logger().debug(msg)
-            twist = self.turtle_twist()
-            self._pub.publish(twist)
+            if self.curr_waypoint is not None and self.waypoints is not None:
+                if self.get_distance(self.pose, self.curr_waypoint) < self.tolerance:
+                    if self.state != State.LAST:
+                        self.curr_waypoint_idx += 1
+                        if self.curr_waypoint_idx >= len(self.waypoints):
+                            self.curr_waypoint_idx = 0
+                            self.curr_waypoint = self.waypoints[0]
+                            self.state = State.LAST
+                        else:
+                            self.curr_waypoint = self.waypoints[self.curr_waypoint_idx]
+                    else:
+                        self.state = State.STOPPED
+                        self.curr_waypoint = self.waypoints[1]
+                        self.curr_waypoint_idx = 1
+                twist = self.turtle_twist()
+                self._pub.publish(twist)
+            else:
+                self.state = State.STOPPED
+                    
+            
 
-    def toggle_callback(self, request, response):
-        if self.state == State.MOVING:
+    async def toggle_callback(self, request, response):
+        if self.state == State.MOVING or self.state == State.LAST:
             self.get_logger().info("Stopping")
             self.state = State.STOPPED
+            self.set_pen_msg.off = 1
+            await self.set_pen.call_async(self.set_pen_msg)
+            
         elif self.state == State.STOPPED:
             self.state = State.MOVING
+            self.set_pen_msg.off = 0
+            await self.set_pen.call_async(self.set_pen_msg)
         return response
 
     async def load_callback(self, request, response):
@@ -78,7 +111,7 @@ class WaypointNode(Node):
         response.distance = 0
         if request.waypoints == None:
             return response
-        
+        self.waypoints = request.waypoints.copy()
         await self.reset.call_async(Empty.Request())
 
         self.set_pen_msg.off = 1
@@ -132,6 +165,7 @@ class WaypointNode(Node):
         await self.teleport.call_async(self.teleport_msg)
         if len(request.waypoints) > 1:
             self.curr_waypoint = request.waypoints[1]
+            self.curr_waypoint_idx = 1
         return response
     
     def feedback_callback(self, pose):
@@ -151,16 +185,37 @@ class WaypointNode(Node):
             Returns:
             Twist - a 2D twist object corresponding to linear/angular velocity
         """
-        if self.state == State.MOVING:
+        if self.state == State.MOVING or self.state == State.LAST:
+            #Establish initial important variables
+            vel_msg = Twist()
+            dist = self.get_distance(self.pose, self.curr_waypoint)
+            linear = dist * self._velocity
+
+            #Calculate relative angle
             dx = self.curr_waypoint.x - self.pose.x
             dy = self.curr_waypoint.y - self.pose.y
-            theta_dir = math.atan(dy/dx)
-            self.get_logger().info(f"Angle is: {theta_dir}")
-            
+            theta_dir = math.atan2(dy, dx)
             omega = theta_dir - self.pose.theta
-        
-            return Twist(linear = Vector3(x = self._velocity, y = 0.0, z = 0.0),
-                    angular = Vector3(x = 0.0, y = 0.0, z = omega))
+            self.get_logger().info(f"Angle is: {omega}")
+            # if (omega > math.pi):
+
+            angle = omega * self._vel_ang
+
+            if omega > 0.01 or omega < -0.01:
+                vel_msg.linear.x = 0.0
+                vel_msg.angular.z = angle
+                return vel_msg
+            else:
+                vel_msg.linear.x = linear
+                vel_msg.angular.z = angle
+                return vel_msg
+            
+        else:
+            return Twist()
+
+            
+
+
 
 
 def main(args=None):
