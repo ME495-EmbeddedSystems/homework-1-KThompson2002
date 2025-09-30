3599d62e63bc91de7b08ee9ad3c796f85d583f23
KThompson2002 <kylezthompson@gmail.com>
2025-09-29 21:45:12 -0500

    Reformatting files

---
+++ inspection.md
@@ -0,0 +1,149 @@
+# CRAZY TURTLE
+Demonstration package for ME450 Embedded Systems in Robotics.
+This README is intentionally vague.
+Figuring out how this package works and filling in the details is part of the
+exercise. Replace the blanks marked with `${ITEM}` with your answer.
+Keep the backticks (\`) but remove the `${}`, so `${ITEM}` becomes `My Answer`.
+Unless otherwise specified, list the command and all arguments that you passed to it.
+
+## Repository Configuration
+1. The `crazy_turtle` git repository consists of the ROS 2 packages `crazy_turtle` and `crazy_turtle_interfaces`.
+2. The package `crazy_turtle` has `<build_type>` of `ament_python`.
+2. The package `crazy_turtle_interfaces` has a `<build_type>` of `ament_cmake`.
+
+
+## Setup Instructions
+1. Build the workspace using `colcon make`.
+2. Initialize the ROS environment (i.e., set the necessary ROS environment variables) by executing `source /opt/ros/kilted/setup.bash`
+3. Make sure no other ROS nodes are running prior to starting by inspecting the results of the ROS command `ros2 node list`.
+3. Run the launchfile `go_crazy_turtle.launch.xml` by executing `ros2 launch go_crazy_turtle.launch.xml`.
+4. When running you can see an interactive visual depiction of the ROS graph using the `ros2 run rqt_graph rqt_graph` command.
+   The ROS graph, including all topics and node labels, looks like:
+   ![The ROS Graph](/HW1/ws/src/homework1/rosgraph.svg)
+
+## Runtime Information
+The `launchfile` from above should be running at all times when executing the following commands.
+If the nodes launched from the `launchfile` are not running, you will get incorrect results.
+
+5. Use the ROS command `ros2 node list` to list all the nodes that are running.
+   The output of the command looks like
+   ```
+   /mover
+   /roving_turtle
+   ```
+6. Use the ROS command `ros2 topic list` to list the topics
+   The output of the command looks like
+   ```
+   /parameter_events
+   /rosout
+   /turtle1/cmd_vel
+   /turtle1/color_sensor
+   /turtle1/pose
+   ```
+
+7. Use the ROS command `ros2 topic hz /turtle1/cmd_vel` to verify that the frequency of
+   the `/turtle1/cmd_vel` topic is `12- Hz`
+
+8. Use the ROS command `ros2 service list` to list the services.
+   The output of the command looks like
+   ```
+   /clear
+   /kill
+   /mover/describe_parameters
+   /mover/get_parameter_types
+   /mover/get_parameters
+   /mover/get_type_description
+   /mover/list_parameters
+   /mover/set_parameters
+   /mover/set_parameters_atomically
+   /reset
+   /roving_turtle/describe_parameters
+   /roving_turtle/get_parameter_types
+   /roving_turtle/get_parameters
+   /roving_turtle/get_type_description
+   /roving_turtle/list_parameters
+   /roving_turtle/set_parameters
+   /roving_turtle/set_parameters_atomically
+   /spawn
+   /switch
+   /turtle1/set_pen
+   /turtle1/teleport_absolute
+   /turtle1/teleport_relative
+   ```
+
+9. Use the ROS command `ros2 service type /switch` to determine the type of the `/switch` service, which is `crazy_turtle_interfaces/serv/Switch`.
+
+10. Use the ROS command `ros2 param list` to list the parameters of all running nodes
+    ```
+    /mover:
+      start_type_description_service
+      use_sim_time
+      velocity
+   /roving_turtle:
+      background_b
+      background_g
+      background_r
+      holonomic
+      qos_overrides./parameter_events.publisher.depth
+      qos_overrides./parameter_events.publisher.durability
+      qos_overrides./parameter_events.publisher.history
+      qos_overrides./parameter_events.publisher.reliability
+      start_type_description_service
+      use_sim_time
+    ```
+
+11. Use the ROS command `ros2 param describe mover velocity` to get information about the `/mover` `velocity` parameter, including its type, description, and constraints
+    ```
+   Parameter name: velocity
+      Type: double
+      Description: The velocity of the turtle
+      Constraints:
+    ```
+
+12. Use the ROS command `ros2 interface show crazy_turtle_interfaces/srv/Switch` to retrieve a template/prototype for entering parameters for the `/switch` service on the command line.
+    ```
+    # use a strange formula to set the new location of the turtle
+   turtlesim_msgs/Pose mixer
+      float32 x
+      float32 y
+      float32 theta
+      float32 linear_velocity
+      float32 angular_velocity
+   ---
+   float64 x # the new x position of the new turtle
+   float64 y # the new y position of the new
+    ```
+
+## Package Exploration
+1. Use the ROS command `ros2 interface package crazy_turtle_interfaces` to list the interface types defined by `crazy_turtle_interfaces`
+   The output of the command looks like
+   ```
+   crazy_turtle_interfaces/srv/Switch
+   ```
+2. Use the ROS command `ros2 pkg executables crazy_turtle` to list the executables included with the `crazy_turtle` package
+   The output of the command looks like
+   ```
+   crazy_turtle mover
+   ```
+
+## Live Interaction
+1. Use the command `ros2 param get /mover velocity` to retrieve the value of the `/mover velocity` parameter, which is `Double value is: 4.5`.
+2. The ROS command to call the `/switch` service is
+    ```
+    ros2 service call /switch crazy_turtle_interfaces/srv/Switch "{ mixer: {x: 1.3, y: 2.1, theta: 0.1, angular_velocity: 3.1, linear_velocity: 4.0}}"
+    ```
+3. The return value of the service is (to two decimal places): `x=3.6499998569488525, y=2.5999999046325684`.
+
+4. The mover node logged the following information in response to the service call:
+   ```
+   Killing turtle1
+   turtle1 is dead :(
+   long live turtle1!
+   turtle1 lives!
+   
+   ```
+5. What happens to the turtle's motion if you use `ros2 param set /mover velocity 12.0` to change `/mover velocity` to 12 while the launchfile is running? `faster`
+6. Use the Linux command `pkill mover` to kill the `/mover` node.
+7. Use the ROS command `ros2 run crazy_turtle mover --ros-args --remap cmd_vel:=/turtle1/cmd_vel -p velocity:=12.0` to start the `/mover` node with a velocity of 12.
+    - HINT: Be sure to remap `cmd_vel` to `/turtle1/cmd_vel`.
+8. What happened to the turtle's velocity after relaunching `mover`? `same`
---
+++ turtle_control/turtle_control/waypoint.py
@@ -0,0 +1,244 @@
+from enum import Enum, auto
+import rclpy
+import math
+
+from rclpy.node import Node
+from std_srvs.srv import Empty
+from turtle_interfaces.srv import Waypoint
+from turtle_interfaces.msg import ErrorMetric
+
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from turtlesim_msgs.srv import SetPen, TeleportAbsolute
+from turtlesim_msgs.msg import Pose
+from geometry_msgs.msg import Twist, Vector3, Point
+
+
+class State(Enum):
+    """ Current state of the system.
+        Determines what the main timer function should be doing on each iteration
+    """
+    MOVING = auto(),
+    LAST = auto()
+    STOPPED = auto(),
+
+class WaypointNode(Node):
+    def __init__(self):
+        super().__init__("waypoint")
+        # declare parameters
+        self.declare_parameter("tolerance", 1.0)
+        self.declare_parameter("frequency", 90.0)
+
+        # Create Parameter
+        self.tolerance = self.get_parameter("tolerance").get_parameter_value().double_value
+        self.freq = self.get_parameter("frequency").get_parameter_value().double_value
+
+        # Establish constant variables
+        self._velocity = 1.0
+        self._vel_ang = 1.0
+        self.timer = self.create_timer(1/self.freq, self.timer_callback)
+        self.my_callback_group = MutuallyExclusiveCallbackGroup()
+        # Create services
+        self.toggle = self.create_service(Empty, "toggle",  self.toggle_callback)
+        self.load = self.create_service(Waypoint, "load", self.load_callback)
+        # Establish initial state
+        self.state = State.STOPPED
+        self.curr_waypoint = None
+        self.curr_waypoint_idx = 0
+        self.waypoints = None
+        self.distance_actual = 0
+        self.distance = 0
+        self.complete_loops = 0
+        self.curr_waypoint_loop = 0
+        self.curr_loop_distance = 0
+        
+        
+        # Creating clients
+        self.reset = self.create_client(Empty, "reset", callback_group=self.my_callback_group)
+        self.set_pen = self.create_client(SetPen, "turtle1/set_pen", callback_group=self.my_callback_group)
+        self.teleport = self.create_client(TeleportAbsolute, "turtle1/teleport_absolute", callback_group=self.my_callback_group)
+
+        # Establish future client storage
+        self.set_pen_msg = SetPen.Request()
+        self.teleport_msg = TeleportAbsolute.Request()
+
+        # Create Subscribers
+        self.pose = Pose()
+        self._feedback = self.create_subscription(Pose, "turtle1/pose", self.feedback_callback, 10)
+
+        # Create Publishers
+        self._pub = self.create_publisher(Twist, "turtle1/cmd_vel", 10)
+        self.metric = self.create_publisher(ErrorMetric, "loop_metrics", 10)
+
+        if not self.reset.wait_for_service(timeout_sec=1.0):
+            raise RuntimeError('Timeout waiting for "reset" service to become available')
+
+        if not self.set_pen.wait_for_service(timeout_sec=1.0):
+            raise RuntimeError('Timeout waiting for "SetPen" service to become available')
+
+        if not self.teleport.wait_for_service(timeout_sec=1.0):
+            raise RuntimeError('Timeout waiting for "Teleport Absolute" service to become available')
+
+
+    def timer_callback(self):
+        if self.state == State.MOVING or self.state == State.LAST:
+            msg = f"moving"
+            self.get_logger().debug(msg)
+            if self.curr_waypoint is not None and self.waypoints is not None:
+                if self.get_distance(self.pose, self.curr_waypoint) < self.tolerance:
+                    if self.state != State.LAST:
+                        self.curr_waypoint_idx += 1
+                        if self.curr_waypoint_idx >= len(self.waypoints):
+                            self.curr_waypoint_idx = 0
+                            self.curr_waypoint = self.waypoints[0]
+                            self.state = State.LAST
+                        else:
+                            self.curr_waypoint = self.waypoints[self.curr_waypoint_idx]
+                    else:
+                        self.complete_loops += 1
+                        self.curr_waypoint_loop += 1
+                        self.state = State.MOVING
+                        self.curr_waypoint = self.waypoints[1]
+                        self.curr_waypoint_idx = 1
+                        if self.curr_waypoint_loop > 1:
+                            self.distance += self.curr_loop_distance
+                        msg = ErrorMetric()
+                        msg.complete_loops = self.complete_loops
+                        msg.actual_distance = self.distance_actual
+                        msg.error = self.distance_actual - self.distance
+                        self.metric.publish(msg)
+                        self.get_logger().info(f"Publishing: {msg.complete_loops}, {msg.actual_distance}, and {msg.error}")
+                twist = self.turtle_twist()
+                self._pub.publish(twist)
+                    
+            
+
+    async def toggle_callback(self, request, response):
+        if self.state == State.MOVING or self.state == State.LAST:
+            self.get_logger().info("Stopping")
+            self.state = State.STOPPED
+            self.set_pen_msg.off = 1
+            await self.set_pen.call_async(self.set_pen_msg)
+            
+        elif self.state == State.STOPPED:
+            self.state = State.MOVING
+            self.set_pen_msg.off = 0
+            await self.set_pen.call_async(self.set_pen_msg)
+        return response
+
+    async def load_callback(self, request, response):
+        self.get_logger().info("Resetting and adding waypoints")
+        self.state = State.STOPPED
+        response.distance = 0
+        if request.waypoints == None:
+            return response
+        self.waypoints = request.waypoints.copy()
+        await self.reset.call_async(Empty.Request())
+
+        self.set_pen_msg.off = 1
+        await self.set_pen.call_async(self.set_pen_msg)
+
+        i = 1
+        for position in request.waypoints:
+            corner1_x = position.x + 0.5
+            corner1_y = position.y + 0.5
+
+            corner2_x = position.x - 0.5
+            corner2_y = position.y - 0.5
+
+            self.teleport_msg.x, self.teleport_msg.y = corner1_x, corner1_y
+            await self.teleport.call_async(self.teleport_msg)
+
+            self.set_pen_msg.off = 0
+            await self.set_pen.call_async(self.set_pen_msg)
+
+            self.teleport_msg.x, self.teleport_msg.y = corner2_x, corner2_y
+            await self.teleport.call_async(self.teleport_msg)
+
+            self.set_pen_msg.off = 1
+            await self.set_pen.call_async(self.set_pen_msg)
+
+            corner1_x = position.x - 0.5
+            corner1_y = position.y + 0.5
+
+            corner2_x = position.x + 0.5
+            corner2_y = position.y - 0.5
+
+            self.teleport_msg.x, self.teleport_msg.y = corner1_x, corner1_y
+            await self.teleport.call_async(self.teleport_msg)
+
+            self.set_pen_msg.off = 0
+            await self.set_pen.call_async(self.set_pen_msg)
+
+            self.teleport_msg.x, self.teleport_msg.y = corner2_x, corner2_y
+            await self.teleport.call_async(self.teleport_msg)
+
+            self.set_pen_msg.off = 1
+            await self.set_pen.call_async(self.set_pen_msg)
+
+            if i < len(request.waypoints):
+                x = i - 1
+                response.distance += self.get_distance(request.waypoints[x], request.waypoints[i])
+                i += 1
+
+
+
+        response.distance += self.get_distance(request.waypoints[0], request.waypoints[i-1])
+        self.curr_waypoint_loop = 0
+        self.teleport_msg.x, self.teleport_msg.y = request.waypoints[0].x, request.waypoints[0].y
+        await self.teleport.call_async(self.teleport_msg)
+        self.get_logger().info(f"Straight line distance: {response.distance}")
+        if len(request.waypoints) > 1:
+            self.curr_waypoint = request.waypoints[1]
+            self.curr_waypoint_idx = 1
+        self.curr_loop_distance = response.distance
+        self.distance += response.distance
+        self.set_pen_msg.off = 0
+        return response
+    
+    def feedback_callback(self, pose):
+        if self.state == State.MOVING or self.state == State.LAST:
+            self.distance_actual += self.get_distance(self.pose, pose)
+        self.pose = pose
+
+
+    def get_distance(self, point1, point2):
+        return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+    
+    def turtle_twist(self):
+        """ Create a twist which moves the turtle proportionally towards the next waypoint """
+        if self.state == State.MOVING or self.state == State.LAST:
+            #Establish initial important variables
+            vel_msg = Twist()
+            dist = self.get_distance(self.pose, self.curr_waypoint)
+            linear = dist * self._velocity
+
+            #Calculate relative angle
+            dx = self.curr_waypoint.x - self.pose.x
+            dy = self.curr_waypoint.y - self.pose.y
+            theta_dir = math.atan2(dy, dx)
+            omega = theta_dir - self.pose.theta
+            # self.get_logger().info(f"Angle is: {omega}")
+            # if (omega > math.pi):
+
+            angle = omega * self._vel_ang
+
+            if omega > 0.01 or omega < -0.01:
+                vel_msg.linear.x = 0.0
+                vel_msg.angular.z = angle
+                return vel_msg
+            else:
+                vel_msg.linear.x = linear
+                vel_msg.angular.z = angle
+                return vel_msg
+            
+        else:
+            return Twist()
+
+            
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = WaypointNode()
+    rclpy.spin(node) 
+    rclpy.shutdown()
+
